6edfcd4ebc24d4988edf35178a8b56f4bca194a8
kristian.kosharov <kristiankosharov@gmail.com>
2018-10-23 18:10:55 +0300

    Added dialog for adding a note

--- app/src/main/java/demo/todosample/ui/MainActivity.kt
+++ app/src/main/java/demo/todosample/ui/MainActivity.kt
@@ -3,6 +3,9 @@ package demo.todosample.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
@@ -38,8 +41,26 @@ class MainActivity : AppCompatActivity() {
                 // TODO Handle add click action
                 logger.info("Handle add")
                 Snackbar.make(binding.todoList, "Add clicked", Snackbar.LENGTH_LONG).show()
+                showCreateTodoDialog()
             }
         }
         return true
+    }
+
+    private fun showCreateTodoDialog() {
+        val builder = AlertDialog.Builder(this)
+        val view = layoutInflater.inflate(R.layout.dialog_add_note, binding.root as ViewGroup, false)
+        val descriptionText = view.findViewById(R.id.todo_description) as EditText
+        builder.setView(view)
+        builder.setPositiveButton(R.string.dialog_button_add) { dialogInterface, i ->
+            logger.info("Handle positive button")
+            Snackbar.make(binding.todoList, "Add dialog clicked", Snackbar.LENGTH_LONG).show()
+
+        }
+        builder.setNegativeButton(android.R.string.cancel) { dialogInterface, i ->
+            logger.info("Handle negative button")
+            Snackbar.make(binding.todoList, "Add dialog cancel", Snackbar.LENGTH_LONG).show()
+        }
+        builder.show()
     }
 }
